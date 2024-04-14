56789b2e804883e27368341fa99f62ce570bda1a
TomasHerg <tomahergenreder@gmail.com>
2024-04-13 23:58:32 -0300

    Se eliminó mi commit anterior
    Borré y volví a instalar el proyecto para solucionar un error y al de la otra rama se eliminó el commit anterior con los cambios que había hecho. Ahora incorporé los vistos en clase

--- app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -1,13 +1,10 @@
 package ayds.songinfo.home.view
-
 import ayds.songinfo.home.model.entities.Song.EmptySong
 import ayds.songinfo.home.model.entities.Song
 import ayds.songinfo.home.model.entities.Song.SpotifySong
-
 interface SongDescriptionHelper {
     fun getSongDescriptionText(song: Song = EmptySong): String
 }
-
 internal class SongDescriptionHelperImpl : SongDescriptionHelper {
     override fun getSongDescriptionText(song: Song): String {
         return when (song) {
@@ -18,8 +15,51 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 }\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                        "Year: ${song.year}"
+                        "Release date: ${song.releaseDate()}"
+
             else -> "Song not found"
         }
     }
+
+    private fun SpotifySong.releaseDate() =
+        when (this.releaseDatePrecision) {
+            "day" -> {
+                val year = this.releaseDate.split("-").first()
+                val month = this.releaseDate.split("-")[1]
+                val day = this.releaseDate.split("-")[2]
+                "$day/$month/$year"
+            }
+
+            "month" -> {
+                val year = this.releaseDate.split("-").first()
+                val month = this.releaseDate.split("-")[1]
+                "${month.toInt().toMonthString()},$year"
+            }
+
+            "year" -> {
+                val isLeapYear = isLeapYear(this.releaseDate.toInt())
+                "${this.releaseDate} ${if (isLeapYear) "(leap year)" else "(not a leap year)"}"
+            }
+
+            else -> ""
+        }
+
+    private fun Int.toMonthString() =
+        when (this) {
+            1 -> "January"
+            2 -> "February"
+            3 -> "March"
+            4 -> "April"
+            5 -> "May"
+            6 -> "June"
+            7 -> "July"
+            8 -> "August"
+            9 -> "September"
+            10 -> "October"
+            11 -> "November"
+            12 -> "December"
+            else -> ""
+        }
+
+    private fun isLeapYear(n: Int) = (n % 4 == 0) && (n % 100 != 0 || n % 400 == 0)
 }
